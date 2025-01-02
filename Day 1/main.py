1b3e304b01bcb7341a1daea5483eebb323c23107
Pasindu Dewviman <pasindudewviman59@gmail.com>
2025-01-02 15:48:41 +0530

    first commit

---
+++ Day 1/main.py
@@ -0,0 +1,4 @@
+print ("welcome to the brand name generator")
+city = input("What is name of the city you grew up in? ")
+pet = input("What is your pet name? ")
+print("Your brand name could be " + city + " " + pet)
